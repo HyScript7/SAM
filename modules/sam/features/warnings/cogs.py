a1dee79b8e9e70bc44efddc03062cf350cd381d8
HyScript7 <hyscript7@seznam.cz>
2025-10-05 20:00:02 +0200

    Feat: Warnings

---
+++ modules/sam/features/warnings/cogs.py
@@ -0,0 +1,116 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+import discord
+from discord.ext import commands
+
+from ...internal import database, logger_config
+from .services import WarnService
+
+logger = logger_config.logger.getChild("warnings")
+
+
+DEFAULT_REASON_WHEN_MISSING = "No reason specified."
+
+
+class Warnings(commands.Cog):
+    def __init__(
+        self, bot: commands.Bot, warn_service: type[WarnService] = WarnService
+    ):
+        self.bot = bot
+        self.svc = warn_service
+
+    @asynccontextmanager
+    async def get_service(self) -> AsyncGenerator[WarnService, None]:
+        async with database.get_session() as session:
+            yield WarnService(session)
+
+    @commands.hybrid_group(
+        name="warnings",
+        usage="warnings ((add <user> [reason]|remove <user> <case_id> [reason])|(list|clear <user>)|view <case_id>)",
+        description="description",
+    )
+    @commands.guild_only()
+    @commands.has_permissions()
+    @commands.cooldown(1, 2, commands.BucketType.member)
+    async def root(self, ctx: commands.Context):
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help(ctx.command)
+
+    @root.command("add")
+    @commands.guild_only()
+    async def _add(
+        self, ctx: commands.Context, user: discord.User, *, reason: str | None = None
+    ):
+        assert ctx.guild is not None
+        if reason is None:
+            reason = DEFAULT_REASON_WHEN_MISSING
+
+        async with self.get_service() as svc:
+            await svc.issue_warning(user.id, ctx.guild.id, ctx.author.id, reason)
+        # TODO: Embed
+        await ctx.send(f"Warned {user.mention} for `{reason}`")
+
+    @root.command("remove")
+    @commands.guild_only()
+    async def _remove(
+        self,
+        ctx: commands.Context,
+        user: discord.User,
+        case_id: int,
+        *,
+        reason: str | None = None,
+    ):
+        assert ctx.guild is not None
+        if reason is None:
+            reason = DEFAULT_REASON_WHEN_MISSING
+        try:
+            async with self.get_service() as svc:
+                await svc.recall_warning(case_id, ctx.guild.id, ctx.author.id, reason)
+        except ValueError as e:
+            # TODO: Embed
+            await ctx.send(f"Cannot remove this warning: {e}")
+        else:
+            # TODO: Embed
+            await ctx.send(
+                f"Removed warning from {user.mention} with reason `{reason}`"
+            )
+
+    @root.command("list")
+    @commands.guild_only()
+    async def _list(self, ctx: commands.Context, user: discord.User):
+        assert ctx.guild is not None
+        async with self.get_service() as svc:
+            warnings = await svc.get_warnings_for_user(user.id, ctx.guild.id)
+        # TODO: Embed, pagination
+        await ctx.send("\n".join(map(str, warnings)))
+
+    @root.command("clear")
+    @commands.guild_only()
+    async def _clear(
+        self, ctx: commands.Context, user: discord.User, *, reason: str | None = None
+    ):
+        assert ctx.guild is not None
+        async with self.get_service() as svc:
+            await svc.clear_warnings_for_user(
+                user.id,
+                ctx.guild.id,
+                ctx.author.id,
+                reason or DEFAULT_REASON_WHEN_MISSING,
+            )
+        # TODO: Embed
+        await ctx.send(f"Cleared warnings for {user.mention} with note `{reason}`")
+
+    @root.command("view")
+    @commands.guild_only()
+    async def _view(self, ctx: commands.Context, case_id: int):
+        assert ctx.guild is not None
+        try:
+            async with self.get_service() as svc:
+                warning = await svc.get_warning(case_id, ctx.guild.id)
+        except ValueError as e:
+            # TODO: Embed
+            await ctx.send(f"Cannot view this warning: {e}")
+        else:
+            # TODO: Embed
+            await ctx.send(str(warning))
---
+++ modules/sam/features/warnings/repositories.py
@@ -0,0 +1,144 @@
+from datetime import datetime
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from ...internal.abstract.abstract_repository import AbstractRepository
+from .models import Warn
+
+
+class WarnRepository(AbstractRepository[Warn]):
+    def __init__(self, session: AsyncSession) -> None:
+        super().__init__(session, Warn)
+
+    async def find_by_guild_id(self, guild_id: int) -> list[Warn]:
+        return (
+            (
+                await self.session.execute(
+                    self.model.select().where(self.model.guild_id == guild_id)
+                )
+            )
+            .scalars()
+            .all()
+        )  # type: ignore
+
+    async def find_by_user_id_and_guild_id(
+        self, user_id: int, guild_id: int
+    ) -> list[Warn]:
+        """
+        Finds all warnings associated with a given user ID. (victim)
+
+        Args:
+            user_id: The ID of the user to find warnings for.
+
+        Returns:
+            A list of Warnings given to a user ID.
+        """
+        return (
+            (
+                await self.session.execute(
+                    self.model.select()
+                    .where(self.model.guild_id == guild_id)
+                    .where(self.model.user_id == user_id)
+                )
+            )
+            .scalars()
+            .all()
+        )  # type: ignore (flex taped to oblivion to silence the linter)
+
+    async def find_by_staff_id_and_guild_id(
+        self, staff_id: int, guild_id: int
+    ) -> list[Warn]:
+        """
+        Finds all warnings given by a given staff ID. (moderator)
+
+        Args:
+            staff_id: The ID of the staff member to find warnings given by.
+
+        Returns:
+            A list of Warnings given by a staff ID.
+        """
+        return (
+            (
+                await self.session.execute(
+                    self.model.select()
+                    .where(self.model.guild_id == guild_id)
+                    .where(self.model.moderator_id == staff_id)
+                )
+            )
+            .scalars()
+            .all()
+        )  # type: ignore
+
+    async def find_by_guild_id_and_after(
+        self, guild_id: int, date: datetime
+    ) -> list[Warn]:
+        """
+        Finds all warnings created after a given date.
+
+        Args:
+            date: The date to find warnings after.
+
+        Returns:
+            A list of Warnings created after the given date.
+        """
+        return (
+            (
+                await self.session.execute(
+                    self.model.select()
+                    .where(self.model.guild_id == guild_id)
+                    .where(self.model.created_at > date)
+                )
+            )
+            .scalars()
+            .all()
+        )  # type: ignore
+
+    async def find_by_guild_id_and_before(
+        self, guild_id: int, date: datetime
+    ) -> list[Warn]:
+        """
+        Finds all warnings created before a given date.
+
+        Args:
+            date: The date to find warnings before.
+
+        Returns:
+            A list of Warnings created before the given date.
+        """
+        return (
+            (
+                await self.session.execute(
+                    self.model.select()
+                    .where(self.model.guild_id == guild_id)
+                    .where(self.model.created_at < date)
+                )
+            )
+            .scalars()
+            .all()
+        )  # type: ignore
+
+    async def find_by_guild_id_and_between(
+        self, guild_id: int, start: datetime, end: datetime
+    ) -> list[Warn]:
+        """
+        Finds all warnings created between two given dates.
+
+        Args:
+            start: The starting date to find warnings between.
+            end: The ending date to find warnings between.
+
+        Returns:
+            A list of Warnings created between the given dates.
+        """
+        return (
+            (
+                await self.session.execute(
+                    self.model.select()
+                    .where(self.model.guild_id == guild_id)
+                    .where(self.model.created_at >= start)
+                    .where(self.model.created_at <= end)
+                )
+            )
+            .scalars()
+            .all()
+        )  # type: ignore
